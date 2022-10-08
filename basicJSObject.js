4560eefc16da42695f2ee50463c431da4692b455
Matthew Greco <mgreco2112@tutanota.com>
2022-10-08 12:30:41 -0400

    refactored to basic function declaration in order to utilize the this keyword

--- basicJSObject.js
+++ basicJSObject.js
@@ -9,12 +9,21 @@ console.log(dog.name);
 console.log(dog.numLegs);
 
 //created method within object
+//the "this" keyword does not work in an ES6 function
 dog = {
     name: "Spot",
     numLegs: 4,
     sayLegs: () => {
       return "This dog has " + dog.numLegs + " legs."
     }
+  };
+  
+  dog.sayLegs();
+
+  dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
   };
   
   dog.sayLegs();
