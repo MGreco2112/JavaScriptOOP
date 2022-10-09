a407d72badc9afdb90a2aaf565812ae1eed1586f
Matthew Greco <mgreco2112@tutanota.com>
2022-10-09 16:00:09 -0400

    filtered between ownProperties and prototypeProperties using hasOwnProperty Object method

--- basicJSObject.js
+++ basicJSObject.js
@@ -90,4 +90,27 @@ dog = {
   // Only change code above this line
   let beagle = new Dog("Snoopy");
 
+  //utilized .hasOwnProperty to filter between propertites within instance of Dog object as ownProperties or prototypeProperties
+
+  // function Dog(name) {
+  //   this.name = name;
+  // }
+  
+  // Dog.prototype.numLegs = 4;
+  
+  // let beagle = new Dog("Snoopy");
+  
+  // let ownProps = [];
+  // let prototypeProps = [];
+  
+  // // Only change code below this line
+  
+  // for (let property in beagle) {
+  //   if (beagle.hasOwnProperty(property)) {
+  //     ownProps.push(property);
+  //   } else {
+  //     prototypeProps.push(property);
+  //   }
+  // }
+
   
