33ef635df3b79a14619abe5ec3a0d6cff61b0a22
Matthew Greco <mgreco2112@tutanota.com>
2022-10-10 11:16:31 -0400

    brought Constructor field into Object.prototype reassignment

--- basicJSObject.js
+++ basicJSObject.js
@@ -144,3 +144,24 @@ dog = {
       console.log(`My name is ${this.name}`)
     }
   };
+
+
+  // When reassigning the Prototype Object, remember to bring the Constructor field value into the new Object
+
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  // Only change code below this line
+  Dog.prototype = {
+    constructor: Dog,
+    numLegs: 4,
+    eat: function() {
+      console.log("nom nom nom");
+    },
+    describe: function() {
+      console.log("My name is " + this.name);
+    }
+  };
+
+  
