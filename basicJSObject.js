90feab0a54ec14da67cd93d697c25f1c45869455
Matthew Greco <mgreco2112@tutanota.com>
2022-10-10 11:47:49 -0400

    utilized supertype prototype to override method in subtype

--- basicJSObject.js
+++ basicJSObject.js
@@ -265,3 +265,23 @@ Dog.prototype.bark = function() {
 beagle = new Dog();
 
 
+//Example of method overriding
+
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+
+Penguin.prototype.fly = function() {
+  return "Alas, this is a flightless bird."
+}
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
